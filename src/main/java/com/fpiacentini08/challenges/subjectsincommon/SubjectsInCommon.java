cdaee0a4c43c59e972e222759f99a2b534aeb4ef
Federico Piacentini <fpiacentini@salesforce.com>
2023-03-28 18:42:10 -0300

    feat(challenges): first coding challenge. Find subjects in common (#1)
    * feat(challenges): first coding challenge. Find subjects in common

---
+++ src/main/java/com/fpiacentini08/challenges/subjectsincommon/SubjectsInCommon.java
@@ -0,0 +1,43 @@
+package com.fpiacentini08.challenges.subjectsincommon;
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.PriorityQueue;
+import java.util.Set;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class SubjectsInCommon {
+
+    public static Map<Set<String>, Set<String>> execute(String[][] listOfStudentsAndSubjects){
+        var subjectsPerStudentMap = new HashMap<String, Set<String>>();
+        for(int i = 0; i < listOfStudentsAndSubjects.length ; i++){
+            var student = listOfStudentsAndSubjects[i][0];
+            var subject = listOfStudentsAndSubjects[i][1];
+            subjectsPerStudentMap.computeIfPresent(student, (key, val) -> addToSetAndReturn(val, subject));
+            subjectsPerStudentMap.computeIfAbsent(student, key -> Set.of(subject));
+        }
+        var response = new HashMap<Set<String>, Set<String>>();
+        var studentsQueue = new PriorityQueue<>(subjectsPerStudentMap.keySet());
+        var studentToMatch = studentsQueue.poll();
+        while(studentToMatch != null){
+            for(var secondStudent : studentsQueue.toArray()){
+                Set<String> studentsSet = new HashSet<>();
+                studentsSet.add(studentToMatch);
+                studentsSet.add((String)secondStudent);
+                var studentToMatchSubjects = subjectsPerStudentMap.get(studentToMatch);
+                var secondStudentSubjects = subjectsPerStudentMap.get(secondStudent);
+                var matchingSubjects = studentToMatchSubjects.stream().filter(secondStudentSubjects::contains).collect(Collectors.toList());
+                response.computeIfAbsent(studentsSet, key -> new HashSet<>(matchingSubjects));
+            }
+            studentToMatch = studentsQueue.poll();
+        }
+        return response;
+    }
+
+    private static Set<String> addToSetAndReturn(Set<String> set, String value){
+        var response = new HashSet<>(set);
+        response.add(value);
+        return response;
+    }
+}
---
+++ src/test/java/com/fpiacentini08/challenges/subjectsincommon/SubjectsInCommonTest.java
@@ -0,0 +1,98 @@
+package com.fpiacentini08.challenges.subjectsincommon;
+
+import org.junit.jupiter.api.Test;
+
+
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class SubjectsInCommonTest {
+
+    @Test
+    void givenArrayWith2StudentsAnd1Subject_whenExecute_shouldReturnMapWithSubjectsInCommonWith1Entry(){
+        final String[][]  testList = {
+                {"12", "Maths"},
+                {"34", "Maths"}
+        };
+
+        var expectedResponseMap = new HashMap<Set<String>, Set<String>>();
+        expectedResponseMap.put(Set.of("12","34"), Set.of("Maths"));
+
+        var responseMap = SubjectsInCommon.execute(testList);
+        assertNotNull(responseMap);
+        assertEquals(1, responseMap.size());
+        responseMap.computeIfPresent(Set.of("12","34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+    }
+
+
+    @Test
+    void givenArrayWith3StudentsAnd1Subject_whenExecute_shouldReturnMapWithSubjectsInCommonWith3Entries(){
+        final String[][]  testList = {
+                {"12", "Maths"},
+                {"17", "Maths"},
+                {"34", "Maths"}
+        };
+
+        var expectedResponseMap = new HashMap<Set<String>, Set<String>>();
+        expectedResponseMap.put(Set.of("17", "34"), Set.of("Maths"));
+        expectedResponseMap.put(Set.of("12", "34"), Set.of("Maths"));
+        expectedResponseMap.put(Set.of("17", "12"), Set.of("Maths"));
+
+        var responseMap = SubjectsInCommon.execute(testList);
+        assertNotNull(responseMap);
+        assertEquals(3, responseMap.size());
+        responseMap.computeIfPresent(Set.of("12", "34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+        responseMap.computeIfPresent(Set.of("12", "17"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+        responseMap.computeIfPresent(Set.of("17", "34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+    }
+
+    @Test
+    void givenArrayWithNoSubjectsInCommon_whenExecute_shouldReturnMapWithSubjectsInCommonEmpty(){
+        final String[][]  testList = {
+                {"12", "Maths"},
+                {"34", "IT"}
+        };
+
+        var expectedResponseMap = new HashMap<Set<String>, Set<String>>();
+        expectedResponseMap.put(Set.of("12", "34"), new HashSet<>());
+
+        var responseMap = SubjectsInCommon.execute(testList);
+        assertNotNull(responseMap);
+        assertEquals(1, responseMap.size());
+        responseMap.computeIfPresent(Set.of("12", "34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+    }
+
+    @Test
+    void givenArrayWithStudentsAndSubjects_whenExecute_shouldReturnMapWithSubjectsInCommon(){
+        final String[][]  testList = {
+                {"12", "Maths"},
+                {"34", "Maths"},
+                {"12", "IT"},
+                {"34", "Chemistry"},
+                {"17", "IT"},
+                {"12", "Chemistry"}
+        };
+
+        var expectedResponseMap = new HashMap<Set<String>, Set<String>>();
+        expectedResponseMap.put(Set.of("12", "34"), Set.of("Maths", "Chemistry"));
+        expectedResponseMap.put(Set.of("12", "17"), Set.of("IT"));
+        expectedResponseMap.put(Set.of("17","34"), Set.of());
+
+        var responseMap = SubjectsInCommon.execute(testList);
+        assertNotNull(responseMap);
+        assertEquals(3, responseMap.size());
+        responseMap.computeIfPresent(Set.of("12", "34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+        responseMap.computeIfPresent(Set.of("12", "17"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+        responseMap.computeIfPresent(Set.of("17","34"), (key, val) -> validateMapValue(key, val, responseMap, expectedResponseMap));
+    }
+
+    private Set<String> validateMapValue(Set<String> key, Set<String> val, Map<Set<String>, Set<String>> responseMap, Map<Set<String>, Set<String>> expectedResponseMap){
+        assertEquals(expectedResponseMap.get(key), responseMap.get(key));
+        return val;
+    }
+}
